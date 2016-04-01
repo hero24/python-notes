9e00d97cb4fb4bf3719a3f2c92e57da1afddc5e5
hero24 <hero24@interia.pl>
2016-04-01 13:50:00 +0100

    Create fibbonacci.py

---
+++ fibbonacci.py
@@ -0,0 +1,14 @@
+# Python 2 Memoization example:
+
+def fib(n,mem=[]):
+    # Example of fibonacci sequence with use of memoization
+    if n < len(mem):
+        return mem[n]
+    else:
+        if n < 2:
+            res = n
+            mem += [res]
+        else:
+            res =  fib(n-1) + fib(n-2)
+            mem += [res]
+        return res
